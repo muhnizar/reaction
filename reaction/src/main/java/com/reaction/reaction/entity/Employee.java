e557097ae5627a4f58afe0971adb0bc606b2d204
HCG\muhammad.nizar01 <muhammad.nizar01@homecredit.co.id>
2018-08-22 12:21:11 +0700

    test

--- reaction/src/main/java/com/reaction/reaction/entity/Employee.java
+++ reaction/src/main/java/com/reaction/reaction/entity/Employee.java
@@ -1,8 +1,30 @@
 package com.reaction.reaction.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * Created by muhammad.nizar01 on 8/22/2018.
  */
+@Data
+@Entity
 public class Employee {
-    
+
+    private @Id @GeneratedValue long id;
+    private String firstName;
+    private String lastName;
+    private String description;
+
+    private Employee(){}
+
+    public Employee(String firstName, String lastName, String description) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.description = description;
+    }
+
+
 }
---
+++ reaction/src/main/java/com/reaction/reaction/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.reaction.reaction.repository;
+
+/**
+ * Created by muhammad.nizar01 on 8/22/2018.
+ */
+public interface EmployeeRepository {
+    
+}
